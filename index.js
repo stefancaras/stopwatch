ac60c4df7740aadd1c6287d70319c17d7ee88188
Stefan Caras <stefan.caras@gmail.com>
2022-09-21 13:31:41 +0300

    update

--- index.js
+++ index.js
@@ -1,42 +1,34 @@
-const hSecond = document.querySelector("#hs");
-const seconds = document.querySelector("#s");
-const minutes = document.querySelector("#m");
+const hs = document.querySelector("#hs");
+const s = document.querySelector("#s");
+const m = document.querySelector("#m");
 const startW = document.querySelector("#start");
 const stopW = document.querySelector("#stop");
 const resetW = document.querySelector("#reset");
 
 let intervalId;
+const array = [0, 0, 0];
 
-let hs = 0;
-let s = 0;
-let m = 0;
-
-const startTimer = () => {
-    const timer = () => {
-        if (hs < 9) {
-            hSecond.textContent = "0" + ++hs;
-        } else {
-            hSecond.textContent = ++hs;
-        }
-        if (hs === 100) {
-            hSecond.textContent = "00";
-            hs = 0;
-            if (s < 9) {
-                seconds.textContent = "0" + ++s;
-            } else {
-                seconds.textContent = ++s;
-            }
-        }
-        if (s === 60) {
-            seconds.textContent = "00";
-            s = 0;
-            if (m < 9) {
-                minutes.textContent = "0" + ++m;
-            } else {
-                minutes.textContent = ++m;
-            }
-        }
+const displayTime = (timeUnit, i) => {
+    if (array[i] < 9) {
+        timeUnit.textContent = "0" + ++array[i];
+    } else {
+        timeUnit.textContent = ++array[i];
     }
+}
+const timer = () => {
+    displayTime(hs, 0)
+    if (array[0] === 100) {
+        hs.textContent = "00";
+        array[0] = 0;
+        displayTime(s, 1)
+    }
+    if (array[1] === 60) {
+        s.textContent = "00";
+        array[1] = 0;
+        displayTime(m, 2)
+    }
+}
+const startTimer = () => {
     clearInterval(intervalId);
     intervalId = setInterval(timer, 10);
 }
@@ -44,6 +36,6 @@ const startTimer = () => {
 startW.addEventListener("click", startTimer);
 stopW.addEventListener("click", () => clearInterval(intervalId));
 resetW.addEventListener("click", () => {
-    hs = s = m = 0; 
-    hSecond.textContent = seconds.textContent = minutes.textContent = "00";
+    array[0] = array[1] = array[2] = 0; 
+    hs.textContent = s.textContent = m.textContent = "00";
 });
